e69fc577b2eda321b9bc6b65e500f6f6c2d0176b
adriananin <adriananin77@gmail.com>
2023-01-11 17:03:29 +0000

    Home Section Complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-
-export default function Button({text}) {
+export default function Button({ text, blue = false }) {
   return (
     <Div>
       <button className={`${blue ? "blue" : ""}`}>{text}</button>
     </Div>
-  )
+  );
+}
 
-}   
+  
 
     const Div = styled.div`
 
